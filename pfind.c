7e304bcfb11bbd05e40ebbdeb408ca1bc5417f27
lilachdaniel <lilachdaniel100@gmail.com>
2022-05-20 16:19:39 +0300

    finally workinggit add pfind.c  need to run tests

--- pfind.c
+++ pfind.c
@@ -49,7 +49,11 @@ void enqueue(queue *q, void *value) {
 	if (is_empty(q)) {
 		q->first = new_node;
 	}
+	else {
+		(q->last)->prev = new_node;
+	}
 	q->last = new_node;
+	
 }
 
 /* Returns the value of the first element of a given queue */
@@ -92,7 +96,9 @@ queue *paths_queue;
 queue *conds_queue;
 
 thrd_t *thrds_arr;
+
 cnd_t *cv_arr;
+cnd_t start_cv;
 
 int num_thrds;
 
@@ -100,37 +106,49 @@ atomic_int num_thrds_waiting;
 atomic_int num_thrds_alive;
 atomic_int num_files_found;
 atomic_int done;
+atomic_int num_thrds_created;
 
 mtx_t paths_mutex;
 mtx_t conds_mutex;
+mtx_t start_mutex;
 
 char *search_term;
 
 /* Wake next sleeping thread up.
    USE AFTER LOCKING conds_mutex !!! */
 void wake_next() {
-	int next_thrd = *((int *)dequeue(conds_queue));
+	long next_thrd = (long)dequeue(conds_queue);
+	printf("waking %ld up\n", next_thrd);
 	cnd_signal(&cv_arr[next_thrd]);
 }
 
-void exit_all_thrds(int id) {
-	printf("thread %d in exit_all_thrds\n", id);
+void exit_all_thrds(long id) {
+	printf("thread %ld in exit_all_thrds\n", id);
 	/* raise flag */
 	done = 1;
 
 	/* wake all threads up */
 	mtx_lock(&conds_mutex);
+	int i = 0;
+	node *curr = conds_queue->first;
+	while (curr != NULL) {
+		printf("%ld\n", (long)(curr->value));
+		curr = curr->next;
+	}
 	while (!is_empty(conds_queue)) {
+		printf("iter %d\n", i);
 		wake_next();
+		i++;
 	}
 	mtx_unlock(&conds_mutex);
 	
 	/* exit */
-	printf("thread %d exiting\n", id);
+	printf("thread %ld exiting\n", id);
 	thrd_exit(0);
 }
 
-void wait_for_tasks(int thrd_id) {
+void wait_for_tasks(long thrd_id) {
+	printf("thread %ld in wait_for_tasks\n", thrd_id);
 	/* check if all threads are sleeping */
 	if (num_thrds_alive - 1 == num_thrds_waiting) {
 		mtx_unlock(&paths_mutex);
@@ -140,19 +158,20 @@ void wait_for_tasks(int thrd_id) {
 	num_thrds_waiting++;
 	
 	mtx_lock(&conds_mutex);
-	enqueue(conds_queue, &thrd_id);
+	enqueue(conds_queue, (void *)thrd_id);
 	mtx_unlock(&conds_mutex);
-	
+	printf("thread %ld is waiting...\n", thrd_id);
 	cnd_wait(&cv_arr[thrd_id], &paths_mutex);
-	
+	mtx_unlock(&paths_mutex);
+	printf("thread %ld done waiting\n", thrd_id);
 	/* thread woke up! */
 	num_thrds_waiting--;
 }
 
 
-void exit_if_really_empty(int id) {
+void exit_if_really_empty(long id) {
 	if (done == 1) {
-		printf("thread %d exiting\n", id);
+		printf("thread %ld exiting\n", id);
 		thrd_exit(0);
 	}
 }
@@ -169,7 +188,7 @@ char *update_path(char *path, char *file_name) {
 }
 
 
-void search_path(char *path, int thrd_id) {
+void search_path(char *path, long thrd_id) {
 	struct dirent *entry;
 	DIR *d;
 	struct stat buf;
@@ -220,8 +239,16 @@ void search_path(char *path, int thrd_id) {
 	
 int thrd_func(void *thrd_id) {
 	char *curr_path;
-	int id = *((int *)thrd_id);
-	printf("thread %d is in thrd_func\n", id);
+	printf("in thrd_func\n");
+	long id = (long)thrd_id;
+	printf("thread %ld is in thrd_func\n", id);
+	
+	/* wait until all threads are created before starting */ 
+	mtx_lock(&start_mutex);
+	num_thrds_created++;
+	cnd_wait(&start_cv, &start_mutex);
+	mtx_unlock(&start_mutex);
+	printf("thread %ld woke up!\n", id);
 	while (1) {
 		mtx_lock(&paths_mutex);
 		if (is_empty(paths_queue)) {
@@ -238,7 +265,6 @@ int thrd_func(void *thrd_id) {
 		mtx_unlock(&paths_mutex);
 		
 		search_path(curr_path, id);
-
 	}
 	return 0; /* everything went fine */
 }
@@ -247,14 +273,26 @@ int thrd_func(void *thrd_id) {
 void init_threads() {
 	thrds_arr = (thrd_t *)malloc(num_thrds * sizeof(thrd_t));
 	// assert malloc
-	for (int i = 0; i < num_thrds; ++i) {
-		printf("creating thread %d\n", i);
-		thrd_create(&thrds_arr[i], thrd_func, &i);
+	for (long i = 0; i < num_thrds; ++i) {
+		printf("creating thread %ld\n", i);
+		thrd_create(&thrds_arr[i], thrd_func, (void *)i);
+	}
+	
+	/* wake all threads up once all threads created */
+	while (1) {
+		mtx_lock(&start_mutex);
+		if (num_thrds_created == num_thrds) {
+			printf("all created\n");
+			cnd_broadcast(&start_cv);
+			mtx_unlock(&start_mutex);
+			break;
+		}
+		mtx_unlock(&start_mutex);
 	}
 }
 
 /* Initializes global variable cv_arr */
-void init_cvs() {
+void init_cv_arr() {
 	cv_arr = (cnd_t *)malloc(num_thrds * sizeof(cnd_t));
 	// assert malloc
 	for (int i = 0; i < num_thrds; ++i) {
@@ -262,8 +300,8 @@ void init_cvs() {
 	}
 }
 
-/* Cleans up all CVs */
-void destroy_cvs() {
+/* Cleans up all CVs in cv_arr */
+void destroy_cv_arr() {
 	for (int i = 0; i < num_thrds; ++i) {
 		cnd_destroy(&cv_arr[i]);
 	}
@@ -274,29 +312,30 @@ void destroy_cvs() {
 
 /* Initialize all global variables */
 void init_global_vars(char *root_path) {
+	/* init atomic vars */
+	num_thrds_created = 0;
+	num_thrds_waiting = 0;
+	num_thrds_alive = num_thrds;
+	num_files_found = 0;
+	done = 0;
+	
 	/* init paths queue */
 	paths_queue = init_queue();
 	enqueue(paths_queue, root_path);
 	
 	/* init cond_queue */
 	conds_queue = init_queue();
-		
-	/* init threads */
-	init_threads();
 	
 	/* init mtxs */
 	mtx_init(&paths_mutex, mtx_plain);
 	mtx_init(&conds_mutex, mtx_plain);
+	mtx_init(&start_mutex, mtx_plain);
 	
 	/* init CVs */
-	init_cvs();
+	init_cv_arr();
 	
-	/* init atomic vars */
-	num_thrds_waiting = 0;
-	num_thrds_alive = num_thrds;
-	num_files_found = 0;
-	done = 0;
-	
+	/* init threads */
+	init_threads();
 }
 
 
@@ -325,7 +364,9 @@ int main(int argc, char *argv[]) {
 	/* clean up */
 	mtx_destroy(&paths_mutex);
 	mtx_destroy(&conds_mutex);
-	destroy_cvs();
+	mtx_destroy(&start_mutex);
+	cnd_destroy(&start_cv);
+	destroy_cv_arr();
 	
 	/* print summery and exit */
 	printf("Done searching, found %d files\n", num_files_found);
